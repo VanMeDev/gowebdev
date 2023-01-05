d71e4eca06921246dd076faaa67497c2ee7cb6f9
Vanya329 <ivanprofi20022@gmail.com>
2023-01-05 17:12:54 +0200

    upgrade

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ const personalMovieDb = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
 
-console.log(personalMovieDb); 
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените его', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+         personalMovieDb.movies[a] = b;
+         console.log('done'); 
+    }else {
+        console.log('error');
+        i--;
+    }
+   
+}
+
+console.log(personalMovieDb);
+
+
