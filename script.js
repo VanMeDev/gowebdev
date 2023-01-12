71eac2c6e4350e5d45e17084667b4abcab234410
Vanya329 <ivanprofi20022@gmail.com>
2023-01-12 14:18:34 +0200

    big object

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
 'use strict';
 
-let numberOfFilms;
-
-function start(){
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
 const personalMovieDb = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function(){
+        personalMovieDb.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDb.count == '' || personalMovieDb.count == null || isNaN(personalMovieDb.count)) {
+            personalMovieDb.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function(){
+        for(let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов', ''),
+                  b = prompt('На сколько оцените его', '');
+            
+            if (a != null && b != null && a != '' && b != '' && a.length < 50){
+                 personalMovieDb.movies[a] = b;
+                 console.log('done'); 
+            }else {
+                console.log('error');
+                i--;
+            }
+           
+        }
+        
+    },
+    detectPersonalLevel: function(){
+        if (personalMovieDb.count < 10) {
+            console.log("немного фильмов");
+        }else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+            console.log("defolt viewer");
+        }else if (personalMovieDb.count >= 30) {
+            console.log("Kinosuperman");
+        }else{
+            console.log("Was error");
+        }
+    },
+    showMyDB: function (hidden) {
+        if(!hidden){
+            console.log(personalMovieDb);
+        }
+    },
+    toggleVisibleMyDb: function(){
+        if(personalMovieDb.privat) {
+            personalMovieDb.privat = false;
+        }else {
+            personalMovieDb.privat = true;
+        }
+    },
+    writeYourGenres: function(){
+        for (let i = 1; i <= 3; i++){
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === '' || genre == null){
+                console.log('Вы ввели некорректные данные или не ввели');
+                i--;
+            }else {
+                personalMovieDb.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDb.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
 };
 
 
 
-
-
-function rememberMyFilms(){
-    for(let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов', ''),
-              b = prompt('На сколько оцените его', '');
-        
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-             personalMovieDb.movies[a] = b;
-             console.log('done'); 
-        }else {
-            console.log('error');
-            i--;
-        }
-       
-    }
-    
-}
-
-//rememberMyFilms();
-
-
-function detectPersonalLevel(){
-    if (personalMovieDb.count < 10) {
-        console.log("немного фильмов");
-    }else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-        console.log("defolt viewer");
-    }else if (personalMovieDb.count >= 30) {
-        console.log("Kinosuperman");
-    }else{
-        console.log("Was error");
-    }
-}
-
-//detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if(!hidden){
-        console.log(personalMovieDb);
-    }
-}
-
-showMyDB(personalMovieDb.privat);
-
-function writeYourGenres(){
-    for (let i = 1; i <= 3; i++){
-        personalMovieDb.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
